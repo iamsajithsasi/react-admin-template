4bb75c69f05774bca54efebef34dded13638540e
sajithsasi <sajithsasi@qburst.com>
2021-12-27 18:10:26 +0530

    Change and forgot password updated

--- src/containers/changepassword.js
+++ src/containers/changepassword.js
@@ -1,9 +1,112 @@
-import React from 'react'
+import React from "react";
+import { Password } from 'primereact/password';
+import { Button } from "primereact/button";
+import { classNames } from "primereact/utils";
+import "../assets/css/login.css";
+
+import * as Yup from "yup";
+import { useFormik, Form, FormikProvider } from "formik";
 
 export default function ChangepasswordPage() {
-    return (
-        <div>
-            change password
+  const ForgotSchema = Yup.object().shape({
+    password: Yup.string().required("Password is required"),
+    confirm_password: Yup.string()
+      .required("Please fill in this field")
+      .oneOf([Yup.ref("password"), null], "Password did'nt match"),
+  });
+
+  const formik = useFormik({
+    initialValues: {
+      password: "",
+      confirm_password: "",
+    },
+    validationSchema: ForgotSchema,
+    onSubmit: (data) => {
+      console.log(data);
+
+      setTimeout(() => {
+        formik.setSubmitting(false);
+      }, 2000);
+    },
+  });
+
+  const { errors, touched, isSubmitting, handleSubmit } = formik;
+
+  return (
+    <div className="form-box">
+      <div className="fullHeight p-ai-center p-d-flex p-jc-center">
+        <div className="shadow card m-3 px-3 py-4 px-sm-4 py-sm-5">
+          <h4 className="text-center">Reset Password</h4>
+          <FormikProvider value={formik}>
+            <Form onSubmit={handleSubmit} className="p-fluid">
+              <div className="p-field">
+                <span className="p-float-label">
+                  <Password
+                    id="password"
+                    name="password"
+                    toggleMask
+                    feedback={false}
+                    value={formik.values.password}
+                    onChange={formik.handleChange}
+                    className={classNames({
+                      "p-invalid": Boolean(touched.password && errors.password),
+                    })}
+                  />
+                  <label
+                    htmlFor="name"
+                    className={classNames({
+                      "p-error": Boolean(touched.password && errors.password),
+                    })}
+                  >
+                    Password*
+                  </label>
+                </span>
+                {Boolean(touched.password && errors.password) && (
+                  <small className="p-error">{formik.errors["password"]}</small>
+                )}
+              </div>
+
+              <div className="p-field">
+                <span className="p-float-label">
+                  <Password
+                    id="confirm_password"
+                    name="confirm_password"
+                    toggleMask
+                    feedback={false}
+                    value={formik.values.confirm_password}
+                    onChange={formik.handleChange}
+                    className={classNames({
+                      "p-invalid": Boolean(touched.confirm_password && errors.confirm_password),
+                    })}
+                  />
+                  <label
+                    htmlFor="name"
+                    className={classNames({
+                      "p-error": Boolean(touched.confirm_password && errors.confirm_password),
+                    })}
+                  >
+                    Confirm Password*
+                  </label>
+                </span>
+                {Boolean(touched.confirm_password && errors.confirm_password) && (
+                  <small className="p-error">{formik.errors["confirm_password"]}</small>
+                )}
+              </div>
+
+              <div className="submitBtnBox">
+                <Button
+                  type="submit"
+                  label="Submit"
+                  iconPos="right"
+                  loading={isSubmitting}
+                  className="mt-4 submitBtn"
+                  disabled={isSubmitting}
+                />
+              </div>
+            </Form>
+          </FormikProvider>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
--- src/containers/forgotpassword.js
+++ src/containers/forgotpassword.js
@@ -1,9 +1,79 @@
-import React from 'react'
+import React from "react";
+import { InputText } from "primereact/inputtext";
+import { Button } from "primereact/button";
+import { classNames } from "primereact/utils";
+import "../assets/css/login.css";
+
+import * as Yup from "yup";
+import { useFormik, Form, FormikProvider } from "formik";
 
 export default function ForgotpasswordPage() {
-    return (
-        <div>
-            forgo password
+  const ForgotSchema = Yup.object().shape({
+    email: Yup.string().required("Email is required"),
+  });
+
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+    },
+    validationSchema: ForgotSchema,
+    onSubmit: (data) => {
+      console.log(data);
+
+      setTimeout(() => {
+        formik.setSubmitting(false);
+      }, 2000);
+    },
+  });
+
+  const { errors, touched, isSubmitting, handleSubmit } = formik;
+
+  return (
+    <div className="form-box">
+      <div className="fullHeight p-ai-center p-d-flex p-jc-center">
+        <div className="shadow card m-3 px-3 py-4 px-sm-4 py-sm-5">
+          <h4 className="text-center">Forgot Password</h4>
+          <FormikProvider value={formik}>
+            <Form onSubmit={handleSubmit} className="p-fluid">
+              <div className="p-field">
+                <span className="p-float-label">
+                  <InputText
+                    id="email"
+                    name="email"
+                    value={formik.values.email}
+                    onChange={formik.handleChange}
+                    className={classNames({
+                      "p-invalid": Boolean(touched.email && errors.email),
+                    })}
+                  />
+                  <label
+                    htmlFor="name"
+                    className={classNames({
+                      "p-error": Boolean(touched.email && errors.email),
+                    })}
+                  >
+                    Email ID*
+                  </label>
+                </span>
+                {Boolean(touched.email && errors.email) && (
+                  <small className="p-error">{formik.errors["email"]}</small>
+                )}
+              </div>
+
+              <div className="submitBtnBox">
+                <Button
+                  type="submit"
+                  label="Submit"
+                  iconPos="right"
+                  loading={isSubmitting}
+                  className="mt-4 submitBtn"
+                  disabled={isSubmitting}
+                />
+              </div>
+            </Form>
+          </FormikProvider>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
